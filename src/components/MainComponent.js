b602145221e8f12a675325069a5979273e95cbd7
David Restrepo <davidrestrepo0107@gmail.com>
2020-06-25 22:17:20 -0500

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,21 +3,31 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props)
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
     render() {
-        const HomePage=()=>{
-            return(
-                <Home/>
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             )
         }
         return (
@@ -25,8 +35,9 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
-                    <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/> {/* con esta tipología se pueden dar props*/}
-                    <Redirect to="/home"/>
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} /> {/* con esta tipología se pueden dar props*/}
+                    <Route exact path="/contactus" component={Contact} />
+                    <Redirect to="/home" />
                 </Switch>
                 <Footer />
             </div>
