80f94c97cabfef4702bb9baeb1766158f648456c
David Restrepo <davidrestrepo0107@gmail.com>
2020-06-27 21:34:31 -0500

    Form

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,60 +1,83 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrumb } from "reactstrap";
+import { Link } from 'react-router-dom'
 
 
+function RenderDish({ dish }) {
+  if (dish != null) {
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  } 
+};
 
- function RenderDish({dish}) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
-  };
+function RenderComments({comments}) {
+  if (comments != null) 
+    return (
+      <ul className="list-unstyled">
+        {comments.map((comment) => {
+          return (
+            <li key={comment.id}>
+              <p>{comment.comment}</p>
+              <p>
+                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+};
 
-  function RenderComments ({comments}) {
-    return comments.map((comment) => {
-      return (
-        <li key={comment.id}>
-          <p>{comment.comment}</p>
-          <p>
-            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
-          </p>
-        </li>
-      );
-    });
-  };
+const Dishdetail = (props) => {
+  const dish = props.dish;
+  const comments = props.comments;
+  if (dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
 
-  const Dishdetail=(props) => {
-    const dish = props.dish;
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to='/menu'>Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>
+              {props.dish.name}
+            </BreadcrumbItem>
+          </Breadcrumb>
 
-    if (dish != null) {
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-5 m-1">
-              <RenderDish dish={dish}/>
-            </div>
-            <div className="col-12 col-md-5 m-1">
-              <h4>Comments</h4>
-              <ul className="list-unstyled">
-                <RenderComments comments={dish.comments}/>
-              </ul>
-            </div>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
           </div>
         </div>
-      );
-    } else {
-      return (
-        <div></div>
-      );
-    }
+
+
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+
+            <RenderComments comments={props.comments} />
+
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div></div>
+    );
   }
+}
 
 
 export default Dishdetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -30,13 +31,24 @@ class Main extends Component {
                 />
             )
         }
+
+        const dishWithId=({match})=>{
+                return(
+                    <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+                        comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}
+                    />
+                );
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} /> {/* con esta tipología se pueden dar props*/}
+                    <Route path="/menu/:dishId" component={dishWithId}/>
                     <Route exact path="/contactus" component={Contact} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} /> 
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
